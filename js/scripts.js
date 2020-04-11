455f5479e236c8849fc81870d2c8a1e7610eb154
vinka123 <ktnvctr123@gmail.com>
2020-04-11 19:29:51 +0300

    Add paragraph text

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 alert('Hello there!This is a simple web app i designed to find your Akan name.');
-function detBday() {
-    document.getElementById('birthday').onsubmit();
-}
-
-
+var days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+if(gender==='Male' && days==='Sunday') {
+ alert('Kwasi');
+} else if(Male && days=='Monday') {
+  alert('Kwadwo');
+} else if(Male && days=='Tuesday') {
+  alert('Kwabena');
+} else if(Male && days=='Wednesday') {
+  alert('Kwaku');
+} else if(Male && days=='Thursday') {
+  alert('Yaw');
+} else if(gender==='Male' && days==='Friday') {
+  alert('Kofi');
+} else if(Male && days=='Saturday') {
+  alert('Kwame');
+}                           
 
